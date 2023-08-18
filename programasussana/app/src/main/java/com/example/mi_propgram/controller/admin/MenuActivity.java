8621c8b04f0105eab5b2aeeecdeeb259a2d581b0
GuillermoSamboni <guillermosamboni0912@gmail.com>
2023-08-18 12:03:03 -0500

    some

---
+++ programasussana/app/src/main/java/com/example/mi_propgram/controller/admin/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.mi_propgram.controller.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mi_propgram.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
